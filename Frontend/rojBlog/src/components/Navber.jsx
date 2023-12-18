614cc486ccfc0c7dc60bbdffaf9bab28ca59b186
md firoj ahmed <mdfirojahmed3052000@gmail.com>
2023-12-18 07:19:29 +0000

    navber

--- Frontend/rojBlog/src/components/Navber.jsx
+++ Frontend/rojBlog/src/components/Navber.jsx
@@ -1,10 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import "react-toastify/dist/ReactToastify.css";
 
 const Navber = () => {
-  
+  const [searchData, setSearchData] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log(searchData);
+  };
   return (
     <>
       <div className="Navber">
@@ -14,19 +19,28 @@ const Navber = () => {
           </Link>
         </div>
         <div className="right">
+          <div className="navfrom">
+            <form onSubmit={onSubmit}>
+              <input
+                name="searchData"
+                value={searchData}
+                placeholder="Search"
+                onChange={(e) => setSearchData(e.target.value)}
+                type="text"
+              />
+            </form>
+          </div>
+
           <Link to={"/register"} className="navitem">
             <h3>Register</h3>
           </Link>
-          <Link to={"/addBlog"} className="navitem">
-            <h3>AddBlog</h3>
-          </Link>
+
           <Link to={"/profile"} className="navitem">
             <h3>Profile</h3>
           </Link>
           <Link to={"/login"} className="navitem">
             <h3>Login</h3>
           </Link>
-          
         </div>
       </div>
     </>
